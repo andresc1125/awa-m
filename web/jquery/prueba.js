2b3d2b56d9f1f6338ebe085b10b6e0bf489cc931
Wilson Salamanca <wilson@wilson-Satellite-M645.(none)>
2014-03-08 23:44:16 -0500

    userBase: jQuery imported and tested

---
+++ web/jquery/prueba.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+				$( ".left-bar" ).click(function() {
+					$( "#left-contet" ).animate({
+						left: "+=50",
+						height: "toggle"
+						}, 5000, function() {.
+							});
+						});
+});
