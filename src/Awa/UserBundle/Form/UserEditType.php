50b9fefade18a362337ca378d391cb91209723db
wass91 <wass91@hotmail.es>
2014-06-25 12:06:01 -0500

    User edit

---
+++ src/Awa/UserBundle/Form/UserEditType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Awa\UserBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class UserEditType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username')
+            ->add('password','repeated',array(
+                'type' => 'password',
+                'invalid_message' => 'password invalid',
+                'options' => array('label' => 'password')))
+            ->add('email','email')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Awa\UserBundle\Entity\User'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'awa_userbundle_usertype';
+    }
+}
