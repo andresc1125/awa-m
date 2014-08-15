25a6458e8ca7347d63c788b32b947d4f3fec1b37
wass91 <wass91@hotmail.es>
2014-08-15 10:50:55 -0500

    ups

---
+++ src/Awa/BussinessBundle/Form/AAplicationDistributorType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Awa\BussinessBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class AAplicationDistributorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('urlDescarga')
+            ->add('price')
+            ->add('currency')
+            ->add('categories')
+            ->add('platform')
+        ;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'Awa\BussinessBundle\Entity\AAplication'
+        ));
+    }
+
+    public function getName()
+    {
+        return 'awa_bussinessbundle_aaplicationtype';
+    }
+}
